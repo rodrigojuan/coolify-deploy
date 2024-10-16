f71e240595755845f8b5041b882d73b370929527
Rodrigo Saes Juan <rodrigo.juan@gmail.com>
2024-10-16 20:21:51 -0300

    Initial commit: FastAPI project for Coolify deployment

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI # type: ignore
+from pydantic import BaseModel # type: ignore
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+
+@app.get("/")
+async def root():
+    return {"message": "Bem-vindo à API simples!"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
